46b9c45ba8e89da12b2e3bb370125bdd4fb2cc5e
MohamDah <mohammedfaiseldhb@gmail.com>
2026-03-14 16:52:18 +0200

    feat: configure firebase_options with flutterfire

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,6 +1,3 @@
-// TODO: Run `flutterfire configure` to generate this file with your Firebase
-// project credentials. Replace this stub before running the app.
-//
 // Example command:
 //   dart pub global activate flutterfire_cli
 //   flutterfire configure
@@ -26,34 +23,45 @@ class DefaultFirebaseOptions {
     }
   }
 
-  // Replace all values below after running `flutterfire configure`.
   static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'REPLACE_ME',
-    appId: 'REPLACE_ME',
-    messagingSenderId: 'REPLACE_ME',
-    projectId: 'REPLACE_ME',
+    apiKey: 'AIzaSyAf4X6Xmr8wqWas8WWNQG2F4vefpWW8eeg',
+    appId: '1:425684716113:web:5f7ba365813537c4f50670',
+    messagingSenderId: '425684716113',
+    projectId: 'fitlife-22bad',
+    authDomain: 'fitlife-22bad.firebaseapp.com',
+    storageBucket: 'fitlife-22bad.firebasestorage.app',
   );
 
+  // Replace all values below after running `flutterfire configure`.
+
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'REPLACE_ME',
-    appId: 'REPLACE_ME',
-    messagingSenderId: 'REPLACE_ME',
-    projectId: 'REPLACE_ME',
+    apiKey: 'AIzaSyBoucHRGG-Fm9HMt0xZpvCcDZxUDud9iDA',
+    appId: '1:425684716113:android:2300fd3440ce0770f50670',
+    messagingSenderId: '425684716113',
+    projectId: 'fitlife-22bad',
+    storageBucket: 'fitlife-22bad.firebasestorage.app',
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'REPLACE_ME',
-    appId: 'REPLACE_ME',
-    messagingSenderId: 'REPLACE_ME',
-    projectId: 'REPLACE_ME',
-    iosBundleId: 'REPLACE_ME',
+    apiKey: 'AIzaSyB0jZtAoK6c5KdZ1I_Am8nd25K93sBRipw',
+    appId: '1:425684716113:ios:8e83787f502244f2f50670',
+    messagingSenderId: '425684716113',
+    projectId: 'fitlife-22bad',
+    storageBucket: 'fitlife-22bad.firebasestorage.app',
+    androidClientId: '425684716113-di2376b703h2qiv5ejn0s3l3haktmiuk.apps.googleusercontent.com',
+    iosClientId: '425684716113-olbjro0qbsfepvhas0pmibd401lmvuc1.apps.googleusercontent.com',
+    iosBundleId: 'com.example.fitlife',
   );
 
   static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'REPLACE_ME',
-    appId: 'REPLACE_ME',
-    messagingSenderId: 'REPLACE_ME',
-    projectId: 'REPLACE_ME',
-    iosBundleId: 'REPLACE_ME',
+    apiKey: 'AIzaSyB0jZtAoK6c5KdZ1I_Am8nd25K93sBRipw',
+    appId: '1:425684716113:ios:8e83787f502244f2f50670',
+    messagingSenderId: '425684716113',
+    projectId: 'fitlife-22bad',
+    storageBucket: 'fitlife-22bad.firebasestorage.app',
+    androidClientId: '425684716113-di2376b703h2qiv5ejn0s3l3haktmiuk.apps.googleusercontent.com',
+    iosClientId: '425684716113-olbjro0qbsfepvhas0pmibd401lmvuc1.apps.googleusercontent.com',
+    iosBundleId: 'com.example.fitlife',
   );
+
 }
